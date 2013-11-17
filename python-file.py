d7a9bcedcd6ecebbc24e1c77e2459c5709c9467d
Steven Burgess <steven.a.burgess@hotmail.com>
2013-11-17 10:27:43 -0800

    adds the python source file
    This is the file a user will copy text out of

---
+++ python-file.py
@@ -0,0 +1,8 @@
+#show a wontan disreguard for pep8 standard
+def messWithPep8Fanboys(fanboys):
+	for fanboy in fanboys:
+		print "use camel case"
+		print "throw some whitespace at the end of lines"   
+		print "sure, a tab too"		
+		if (notMadYet):
+			print remind
